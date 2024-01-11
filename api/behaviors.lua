bc4ae94b53a237a12306fd15ca656622cd5eee75
flux <25628292+fluxionary@users.noreply.github.com>
2024-01-11 11:20:48 -0800

    refactor replacement logic to be more legible

--- api/behaviors.lua
+++ api/behaviors.lua
@@ -142,29 +142,30 @@ local function replace(self, pos0)
 	end
 	local radius = pprops.radius or 0
 	pos0 = pos0:round()
-	local placer
+	local try_place
 	if minetest.is_player(self._source_obj) then
-		placer = self._source_obj
+		local placer = self._source_obj
+		local itemstack = ItemStack(replacement.name)
+		local param2 = replacement.param2
+		function try_place(pos)
+			local pointed_thing = { type = "node", under = pos, above = pos }
+			-- second argument is placed position or nil
+			if select(2, minetest.item_place_node(itemstack, placer, pointed_thing, param2)) then
+				return true
+			end
+		end
+	else
+		function try_place(pos)
+			return minetest.place_node(pos, replacement)
+		end
 	end
 	local placed = false
 	for x = -radius, radius do
 		for y = -radius, radius do
 			for z = -radius, radius do
 				local pos = pos0:offset(x, y, z)
-				if minetest.get_node(pos).name == target then
-					if placer then
-						local itemstack = ItemStack(replacement.name)
-						local pointed_thing = { type = "node", under = pos, above = pos }
-						local param2 = replacement.param2
-						-- second argument is placed position or nil
-						if select(2, minetest.item_place_node(itemstack, placer, pointed_thing, param2)) then
-							placed = true
-						end
-					else
-						if minetest.place_node(pos, replacement) then
-							placed = true
-						end
-					end
+				if minetest.get_node(pos).name == target and try_place(pos) then
+					placed = true
 				end
 			end
 		end
