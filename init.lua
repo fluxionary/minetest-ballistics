398da2a092c7931d5566cb4ac266342ab81e1371
flux <25628292+fluxionary@users.noreply.github.com>
2024-01-28 13:50:35 -0800

    actually execute the util file

--- init.lua
+++ init.lua
@@ -2,5 +2,6 @@ futil.check_version({ year = 2024, month = 1, day = 15 }, "need make_registratio
 
 ballistics = fmod.create()
 
+ballistics.dofile("util")
 ballistics.dofile("api", "init")
 ballistics.dofile("test_tools", "init")
